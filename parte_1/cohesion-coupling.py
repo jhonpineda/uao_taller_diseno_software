9a5ead4794b71bee2720fbee6c82d4339d143a37
Anakin Skywalker <jhon.pineda@musicar.com>
2022-08-20 18:28:40 -0500

    version 20220822 tarea 1 refactorizada

--- parte_1/cohesion-coupling.py
+++ parte_1/cohesion-coupling.py
@@ -1,36 +1,36 @@
 import string
 import random
 
-class VehicleRegistry:
+class RegistroVehiculo:
 
-    def generate_vehicle_id(self, length):
+    def generar_vehiculo_id(self, length):
         return ''.join(random.choices(string.ascii_uppercase, k=length))
 
-    def generate_vehicle_license(self, id):
+    def generar_licencia_vehiculo(self, id):
         return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
 
 
-class Application:
+class Aplicacion:
 
-    def register_vehicle(self, brand: string):
+    def registrar_vehiculo(self, brand: string):
         # create a registry instance
-        registry = VehicleRegistry()
+        registro = RegistroVehiculo()
 
         # generate a vehicle id of length 12
-        vehicle_id = registry.generate_vehicle_id(12)
+        vehiculo_id = registro.generar_vehiculo_id(12)
 
         # now generate a license plate for the vehicle
         # using the first two characters of the vehicle id
-        license_plate = registry.generate_vehicle_license(vehicle_id)
+        placa_matricula = registro.generar_licencia_vehiculo(vehiculo_id)
 
         # compute the catalogue price
-        catalogue_price = 0
+        catalogo_precio = 0
         if brand == "Tesla Model 3":
-            catalogue_price = 60000
+            catalogo_precio = 60000
         elif brand == "Volkswagen ID3":
-            catalogue_price = 35000
+            catalogo_precio = 35000
         elif brand == "BMW 5":
-            catalogue_price = 45000
+            catalogo_precio = 45000
 
         # compute the tax percentage (default 5% of the catalogue price, except for electric cars where it is 2%)
         tax_percentage = 0.05
@@ -38,14 +38,14 @@ class Application:
             tax_percentage = 0.02
 
         # compute the payable tax
-        payable_tax = tax_percentage * catalogue_price
+        payable_tax = tax_percentage * catalogo_precio
 
         # print out the vehicle registration information
         print("Registration complete. Vehicle information:")
         print(f"Brand: {brand}")
-        print(f"Id: {vehicle_id}")
-        print(f"License plate: {license_plate}")
+        print(f"Id: {vehiculo_id}")
+        print(f"License plate: {placa_matricula}")
         print(f"Payable tax: {payable_tax}")
 
-app = Application()
-app.register_vehicle("Volkswagen ID3")
+app = Aplicacion()
+app.registrar_vehiculo("Volkswagen ID3")
--- parte_1/cohesion-coupling2.py
+++ parte_1/cohesion-coupling2.py
@@ -2,72 +2,72 @@ import string
 import random
 
 
-class VehicleInfo:
+class VehiculoInfo:
 
-    def __init__(self, brand, electric, catalogue_price):
+    def __init__(self, brand, electric, catalogo_precio):
         self.brand = brand
         self.electric = electric
-        self.catalogue_price = catalogue_price
+        self.catalogo_precio = catalogo_precio
 
     def compute_tax(self):
         tax_percentage = 0.05
         if self.electric:
             tax_percentage = 0.02
-        return tax_percentage * self.catalogue_price
+        return tax_percentage * self.catalogo_precio
 
     def print(self):
         print(f"Brand: {self.brand}")
         print(f"Payable tax: {self.compute_tax()}")
 
 
-class Vehicle:
+class Vehiculo:
 
-    def __init__(self, id, license_plate, info):
+    def __init__(self, id, placa_matricula, info):
         self.id = id
-        self.license_plate = license_plate
+        self.placa_matricula = placa_matricula
         self.info = info
 
     def print(self):
         print(f"Id: {self.id}")
-        print(f"License plate: {self.license_plate}")
+        print(f"License plate: {self.placa_matricula}")
         self.info.print()
 
 
-class VehicleRegistry:
+class VehiculoRegistro:
 
     def __init__(self):
-        self.vehicle_info = {}
-        self.add_vehicle_info("Tesla Model 3", True, 60000)
-        self.add_vehicle_info("Volkswagen ID3", True, 35000)
-        self.add_vehicle_info("BMW 5", False, 45000)
-        self.add_vehicle_info("Tesla Model Y", True, 75000)
+        self.Vehiculo_info = {}
+        self.add_Vehiculo_info("Tesla Model 3", True, 60000)
+        self.add_Vehiculo_info("Volkswagen ID3", True, 35000)
+        self.add_Vehiculo_info("BMW 5", False, 45000)
+        self.add_Vehiculo_info("Tesla Model Y", True, 75000)
 
-    def add_vehicle_info(self, brand, electric, catalogue_price):
-        self.vehicle_info[brand] = VehicleInfo(brand, electric, catalogue_price)
+    def add_Vehiculo_info(self, brand, electric, catalogo_precio):
+        self.Vehiculo_info[brand] = VehiculoInfo(brand, electric, catalogo_precio)
 
-    def generate_vehicle_id(self, length):
+    def generate_Vehiculo_id(self, length):
         return ''.join(random.choices(string.ascii_uppercase, k=length))
 
-    def generate_vehicle_license(self, id):
+    def generate_Vehiculo_license(self, id):
         return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
 
-    def create_vehicle(self, brand):
-        id = self.generate_vehicle_id(12)
-        license_plate = self.generate_vehicle_license(id)
-        return Vehicle(id, license_plate, self.vehicle_info[brand])
+    def create_Vehiculo(self, brand):
+        id = self.generate_Vehiculo_id(12)
+        placa_matricula = self.generate_Vehiculo_license(id)
+        return Vehiculo(id, placa_matricula, self.Vehiculo_info[brand])
 
 
-class Application:
+class Aplicacion:
 
-    def register_vehicle(self, brand: string):
+    def register_Vehiculo(self, brand: string):
         # create a registry instance
-        registry = VehicleRegistry()
+        registry = VehiculoRegistro()
 
-        vehicle = registry.create_vehicle(brand)
+        Vehiculo = registry.create_Vehiculo(brand)
 
-        # print out the vehicle information
-        vehicle.print()
+        # print out the Vehiculo information
+        Vehiculo.print()
 
 
-app = Application()
-app.register_vehicle("Volkswagen ID3")
+app = Aplicacion()
+app.register_Vehiculo("Volkswagen ID3")
